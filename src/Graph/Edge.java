8c6e8f37996852b381be0377a1250e9e9c767110
Felipe Cruz <felp.cruz.comp@gmail.com>
2024-12-04 20:42:53 -0300

    :wrench: chore: Mudança de pasta e adição de atributos

---
+++ src/Graph/Edge.java
@@ -0,0 +1,41 @@
+package Graph;
+public class Edge {
+    String name;
+    Vertex source;
+    Vertex target;
+    int weight;
+    public Edge(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public void printInfo(){
+        System.out.print("[" + getSource().getName() + ", " + getTarget().getName() + "]");
+    }
+
+    public void printStatus() {
+        System.out.println("\n\n-----------------------EDGE--" + getName() + "-----------------------");
+        System.out.println("- SOURCE: " + source.getName());
+        System.out.println("- TARGET: " + target.getName());
+        System.out.println("-------------------------");
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Vertex getSource() {
+        return source;
+    }
+    public void setSource(Vertex source) {
+        this.source = source;
+    }
+    public Vertex getTarget() {
+        return target;
+    }
+    public void setTarget(Vertex target) {
+        this.target = target;
+    }
+}
